5eeef058d9076a2ec1ff8fb7133dc3d9b6b18f4e
EwarJames <ewaarjames@gmail.com>
2020-12-22 15:05:29 +0000

    Header file

--- monty.h
+++ monty.h
@@ -4,9 +4,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
+#include <stdbool.h>
 #include <ctype.h>
 
-extern int n, number;
+/**
+ * struct stack_s - doubly linked list representation of a stack (or queue)
+ * @n: integer
+ * @prev: points to the previous element of the stack (or queue)
+ * @next: points to the next element of the stack (or queue)
+ *
+ * Description: doubly linked list node structure
+ * for stack, queues, LIFO, FIFO Holberton project
+ */
+typedef struct arg_s
+{
+	int arg;
+	int flag;
+} arg_t;
+
+extern arg_t arg;
 
 /**
  * struct stack_s - doubly linked list representation of a stack (or queue)
@@ -38,41 +55,61 @@ typedef struct instruction_s
 	void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
 
-void usage_error(void);
-void open_error(char **);
-void invalidInstruction_error(char *invInstruction, unsigned int line);
-void not_int_err(unsigned int line);
-void malloc_error(void);
-void pint_error(unsigned int line);
-void pop_error(unsigned int line);
-void swap_error(unsigned int line);
-void add_error(unsigned int line);
-void sub_error(unsigned int line);
-void div_error(unsigned int line);
-void div_error2(unsigned int line);
-void mul_error(unsigned int line);
-void mod_error(unsigned int line);
-void pchar_error(unsigned int line);
-void pchar_error2(unsigned int line);
-void open_and_read(char **argv);
-int is_number(char *token);
-int is_comment(char *token, int line_counter);
-void (*get_op_code(char *token, unsigned int line)) (stack_t **, unsigned int);
-void func_push(stack_t **stack, unsigned int line_number);
-void func_pall(stack_t **stack, unsigned int line_number);
-void free_stack(stack_t *stack);
-void func_pint(stack_t **stack, unsigned int line_number);
-void func_pop(stack_t **stack, unsigned int line_number);
-void func_swap(stack_t **stack, unsigned int line);
-void func_add(stack_t **stack, unsigned int line);
-void func_sub(stack_t **stack, unsigned int line_number);
-void func_div(stack_t **stack, unsigned int line_number);
-void func_mul(stack_t **stack, unsigned int line);
-void func_mod(stack_t **stack, unsigned int line_number);
-void func_rotl(stack_t **stack, unsigned int line_number);
-void func_rotr(stack_t **stack, unsigned int line_number);
-void func_nop(stack_t **stack, unsigned int line);
-void func_pchar(stack_t **stack, unsigned int line_number);
-void func_pstr(stack_t **stack, unsigned int line_number);
+/**
+ * struct line - contents of line and corresponding number
+ * @contents: array of tokens read from the line
+ * @number: the line number
+ *
+ * Description: contents of a line and corresponding number
+ */
+typedef struct line
+{
+	unsigned int number;
+	char **content;
+} line_t;
+
+/**
+ * struct stack_s - doubly linked list representation of a stack (or queue)
+ * @n: integer
+ * @prev: points to the previous element of the stack (or queue)
+ * @next: points to the next element of the stack (or queue)
+ *
+ * Description: doubly linked list node structure
+ * for stack, queues, LIFO, FIFO Holberton project
+ */
+typedef struct meta_s
+{
+	char *buf;
+	stack_t *stack;
+	FILE *file;
+} meta_t;
+
+void (*get_op_func(line_t line, meta_t *meta))(stack_t **, unsigned int);
+int _isalpha(int c);
+void parsefile(FILE *file);
+void parseline(line_t *line, char *buffer);
+bool comment_check(line_t line);
+void push_check(line_t line, meta_t *meta, char *opcode);
+void push(stack_t **stack, unsigned int nline);
+void pall(stack_t **stack, unsigned int nline);
+void pint(stack_t **stack, unsigned int nline);
+void pop(stack_t **stack, unsigned int nline);
+void swap(stack_t **stack, unsigned int nline);
+void nop(stack_t **stack, unsigned int nline);
+void rotl(stack_t **stack, unsigned int nline);
+void rotlop(stack_t **stack, unsigned int nline);
+void rotrop(stack_t **stack, unsigned int nline);
+void pchar(stack_t **stack, unsigned int nline);
+void pstr(stack_t **stack, unsigned int nline);
+void free_stack(stack_t **stack);
+void nop(stack_t **stack, unsigned int nline);
+void qpush(stack_t **stack, unsigned int nline);
+void addqu(stack_t **stack, unsigned int nline);
+void addst(stack_t **stack, unsigned int nline);
+void subop(stack_t **stack, unsigned int nline);
+void addop(stack_t **stack, unsigned int nline);
+void divop(stack_t **stack, unsigned int nline);
+void mulop(stack_t **stack, unsigned int nline);
+void modop(stack_t **stack, unsigned int nline);
 
 #endif
